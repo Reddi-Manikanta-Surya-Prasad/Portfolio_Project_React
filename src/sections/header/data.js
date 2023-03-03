acd0437aecca7eecb7514a71bc215964c553cc7f
Reddi-Manikanta-Surya-Prasad <reddimani14@gmail.com>
2023-03-03 09:15:10 +0530

    committing

---
+++ src/sections/header/data.js
@@ -0,0 +1,13 @@
+import {AiOutlineInstagram} from 'react-icons/ai'
+import {AiOutlineTwitter} from 'react-icons/ai'
+import {FaLinkedin} from 'react-icons/fa'
+import {AiFillGithub} from 'react-icons/ai'
+
+const data = [
+    {id: 1, link: 'https://www.instagram.com/reddy.mani.336/', icon: <AiOutlineInstagram/>},
+    {id: 2, link: 'https://twitter.com/Reddi8Mani', icon: <AiOutlineTwitter/>},
+    {id: 3, link: 'https://www.linkedin.com/in/manikantareddi/', icon: <FaLinkedin/>},
+    {id: 4, link: 'https://github.com/Reddi-Manikanta-Surya-Prasad', icon: <AiFillGithub/>}
+]
+
+export default data
---
+++ src/sections/services/data.js
@@ -0,0 +1,23 @@
+import {SiAdobexd} from 'react-icons/si'
+import {RiReactjsLine} from 'react-icons/ri'
+import {FaServer} from 'react-icons/fa'
+import {DiAws} from 'react-icons/di'
+
+
+const data = [
+    {
+        id: 1, icon: <SiAdobexd/>, title: 'UI/UX Design', desc: "My designs are modern and intuitive. I use industry-standard rules to make sure your users have fun using your product."
+    },
+    {
+        id: 2, icon: <RiReactjsLine/>, title: 'Frontend Development', desc: "Your product will look good and will be accessible on all devices, including mobile phones, tablets, and desktop."
+    },
+    {
+        id: 3, icon: <FaServer/>, title: 'Backend Development', desc: "The security of your business/product is taken seriously right from the start of the project. I will make sure your website/app is secure from attacks."
+    },
+    {
+        id: 4, icon: <DiAws/>, title: 'AWS DevOps', desc: 'Unlike other developers in the market, I am also a certified AWS DevOps Engineer Professional that I can wrok on operations without any extra cost to you.'
+    }
+]
+
+
+export default data
